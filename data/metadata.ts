5703867b605820130373ebb0ccff445ea2a604ee
Jared <jaredblase@gmail.com>
2023-01-07 18:46:46 +0800

    add: header disclosure

--- data/metadata.ts
+++ data/metadata.ts
@@ -4,6 +4,6 @@ export const description = `
 Hi! I'm Jared, a senior Computer Science student in De La Salle University - Manila.
 I have experience in web development, object-oriented programming, database design, 
 and software engineering.
-`.replace('\n', ' ').trim()
+`.replace(/[\r\n]/gm, ' ').replace('  ', ' ').trim()
 
 export const link = 'https://jaredblase.github.io'
