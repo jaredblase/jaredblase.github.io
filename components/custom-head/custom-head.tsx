4ef2ba65f72f90736a7f35f8d824e91ea9ffb166
Jared <jaredblase@gmail.com>
2023-01-07 17:30:45 +0800

    update: metadata image addresses

--- components/custom-head/custom-head.tsx
+++ components/custom-head/custom-head.tsx
@@ -13,20 +13,20 @@ export function CustomHead({ title, description, link }: CustomHeadProps) {
 			<meta name="description" content={description} />
 			<meta name="keywords" content="portfolio, developer, Jared Sy, Jared Blase Sy" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<link rel="icon" href="/favicon.ico" />
+			<link rel="icon" href="/favicon.jpg" />
 
 			<meta name="og:title" content={title} />
 			<meta name="og:url" content={link} />
 			<meta name="og:type" content="profile" />
 			<meta name="og:description" content={description} />
-			<meta name="og:image" content="https://jaredblase.github.io/assets/og_image.avif" />
+			<meta name="og:image" content={`${link}/images/og_image.jpg`} />
 
 			<meta name="twitter:title" content={title} />
 			<meta name="twitter:url" content={link} />
-			<meta name="twitter:image" content="https://jaredblase.github.io/assets/og_image.avif" />
+			<meta name="twitter:image" content={`${link}/images/og_image.jpg`} />
 			<meta name="twitter:description" content={description} />
 
-			<link rel="shortcut icon" type="image/jpg" href="https://jaredblase.github.io/assets/favicon.jpg" />
+			<link rel="shortcut icon" type="image/jpg" href={`${link}/images/favicon.jpg`} />
 		</Head>
 	)
 }
--- data/metadata.ts
+++ data/metadata.ts
@@ -1,9 +1,9 @@
 export const title = 'Jared Sy | Developer Portfolio'
 
 export const description = `
-Hi! I'm Jared, a junior Computer Science student in De La Salle University - Manila.
+Hi! I'm Jared, a senior Computer Science student in De La Salle University - Manila.
 I have experience in web development, object-oriented programming, database design, 
 and software engineering.
-`
+`.replace('\n', ' ').trim()
 
 export const link = 'https://jaredblase.github.io'
